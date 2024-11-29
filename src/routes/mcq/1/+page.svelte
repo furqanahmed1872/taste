39aea5ad0f8da39aacc9df4947e279bf2166285b
Your Name <abdullaharifsalimee@gmail.com>
2024-11-29 21:03:08 +0500

    ok

--- src/routes/mcq/1/+page.svelte
+++ src/routes/mcq/1/+page.svelte
@@ -1,82 +1,61 @@
-<script>
-  let clickedDiv = null;
+  <!-- Background Image (fixed position) -->
+  <style>
 
-  function handleDivClick(divType) {
-    clickedDiv = divType;
-  }
-</script>
 
-<!-- Background and overlay -->
-<div class="fixed inset-0 bg-cover bg-center blur-sm" style="background-image: url('../back.jpg');"></div>
-<div class="fixed inset-0 bg-custom-dark-red opacity-90"></div>
 
-<!-- Main Content -->
-<div class="relative z-10 flex flex-col items-center text-white py-6">
-  <div class="flex justify-center">
-    <img src="../logo.png" alt="Logo" class="h-36 w-40" />
-  </div>
+  </style>
+  
+  
+  <div class="fixed inset-0 bg-cover bg-center blur-sm" style="background-image: url('../back.jpg');"></div>
+  
+  <!-- Color Overlay -->
+  <div class="fixed inset-0 bg-custom-dark-red opacity-90"></div>
 
-  <div class="text-5xl text-custom-grey font-poiret bg-custom-dark-brown opacity-70 w-full text-center p-1 my-2">
-    "Discover Your Movie Taste!"
-  </div>
-
-  <div class="grid grid-cols-2 grid-rows-6 2xl 2xl:w-1/2 xl:w-2/3 m-8 lg:w-3/4">
-    <div class="row-span-6 bg-black opacity-70 rounded-3xl font-poiret text-5xl justify-center items-center grid p-8 text-center">
-      “Select the Time period for movie”
+  <!-- Main Content -->
+  <div class="relative z-10 flex flex-col items-center text-white py-6">
+    <!-- Logo -->
+    <div class="flex justify-center">
+      <img src="../logo.png" alt="Logo" class="h-36 w-40" />
     </div>
 
-    <div class="row-span-6 my-7 grid gap-4 overflow-hidden relative">
-      <!-- Button group with conditional width applied to each button -->
-      <button
-        on:click={() => handleDivClick('movies')}
-        class={`animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50 translate-x-[-100%] rounded-e-xl ${
-          clickedDiv === 'movies' ? 'w-full' : 'w-[85%]'
-        }`}
-      >
-        Released 5 years ago
-      </button>
-      
-      <button
-        on:click={() => handleDivClick('released10')}
-        class={`animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50 translate-x-[-100%] rounded-e-xl ${
-          clickedDiv === 'released10' ? 'w-full' : 'w-[85%]'
-        }`}
-      >
-        Released 10 years ago
-      </button>
-      <button
-        on:click={() => handleDivClick('released15')}
-        class={`animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50 translate-x-[-100%] rounded-e-xl ${
-          clickedDiv === 'released15' ? 'w-full' : 'w-[85%]'
-        }`}
-      >
-        Released 15 years ago
-      </button>
-      <button
-        on:click={() => handleDivClick('released20')}
-        class={`animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50 translate-x-[-100%] rounded-e-xl ${
-          clickedDiv === 'released20' ? 'w-full' : 'w-[85%]'
-        }`}
-      >
-        Released 20 years ago
-      </button>
-      <button
-        on:click={() => handleDivClick('dontMatter')}
-        class={`animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50 translate-x-[-100%] rounded-e-xl ${
-          clickedDiv === 'dontMatter' ? 'w-full' : 'w-[85%]'
-        }`}
-      >
-        Doesn't matter
-      </button>
+    <!-- Main Text -->
+    <div class="text-5xl text-custom-grey font-poiret bg-custom-dark-brown opacity-70 w-full text-center p-1 my-2">
+      "Discover Your Movie Taste!"
     </div>
-  </div>
 
-  <!-- Conditionally render the NEXT button -->
-  {#if clickedDiv}
+    <!-- Grid Content -->
+    <div class="grid grid-cols-2 grid-rows-6 2xl 2xl:w-1/2 xl:w-2/3 m-8 lg:w-3/4">
+      <!-- Black Box -->
+      <div class="row-span-6 bg-black opacity-70 rounded-3xl font-poiret text-5xl justify-center items-center grid p-8 text-center">
+        “Select the Time period for movie”
+      </div>
+
+      <!-- Container for Red Bars -->
+      <div class="row-span-6 my-7 grid gap-4 overflow-hidden relative ">
+        <!-- Animated Bars -->
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50 translate-x-[-100%] rounded-e-xl">
+          Relased 5 years ago
+        </button>
+        
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50 translate-x-[-100%] rounded-e-xl">
+          Relased 10 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50 translate-x-[-100%] rounded-e-xl">
+          Relased 15 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50 translate-x-[-100%] rounded-e-xl">
+          Relased 20 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50 translate-x-[-100%] rounded-e-xl">
+           doesn't matter
+        </button>
+       
+      </div>
+    </div>
+
     <a 
-      href="/mcq/2"
-      class="bg-black opacity-50 font-poiret text-5xl p-6 rounded-2xl m-4 hover:opacity-100">
+    href="/mcq/3"
+    class="bg-black opacity-50 font-poiret text-5xl p-6 rounded-2xl m-4 hover:opacity-100">
       NEXT
     </a>
-  {/if}
-</div>
+  </div>
--- src/routes/mcq/2/+page.svelte
+++ src/routes/mcq/2/+page.svelte
@@ -24,32 +24,88 @@
     </div>
 
     <!-- Grid Content -->
-    <div class="grid grid-cols-2 grid-rows-6 2xl 2xl:w-1/2 xl:w-2/3 m-8 lg:w-3/4">
+    <div class="grid grid-cols-3 grid-rows-10 2xl 2xl:w-1/2 xl:w-2/3 m-8 lg:w-3/4  ">
+      
+      <div class="row-span-10 my-7 flex flex-col gap-4  justify-stretch ">
+        <!-- Animated Bars -->
+         <!-- Animated Bars -->
+         <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+          Relased 5 years ago
+        </button>
+        
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+          Relased 10 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+          Relased 15 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+          Relased 20 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+           doesn't matter
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+          Relased 5 years ago
+        </button>
+        
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+          Relased 10 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+          Relased 15 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+          Relased 20 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+           doesn't matter
+        </button>
+       
+      </div>
+   
+      
       <!-- Black Box -->
-      <div class="row-span-6 bg-black opacity-70 rounded-3xl font-poiret text-5xl justify-center items-center grid p-8 text-center">
+      <div class="row-span-10 bg-black opacity-70 rounded-3xl font-poiret text-5xl justify-center items-center grid p-8 text-center">
         “Select the Time period for movie”
       </div>
 
       <!-- Container for Red Bars -->
-      <div class="row-span-6 my-7 grid gap-4 overflow-hidden relative ">
+           
+      <div class="row-span-10 my-7 grid gap-4 overflow-hidden relative">
         <!-- Animated Bars -->
-        <div class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50 translate-x-[-100%] rounded-e-xl">
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
           Relased 5 years ago
-        </div>
+        </button>
         
-        <div class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50 translate-x-[-100%] rounded-e-xl">
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
           Relased 10 years ago
-        </div>
-        <div class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50 translate-x-[-100%] rounded-e-xl">
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
           Relased 15 years ago
-        </div>
-        <div class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50 translate-x-[-100%] rounded-e-xl">
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
           Relased 20 years ago
-        </div>
-        <div class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50 translate-x-[-100%] rounded-e-xl">
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
            doesn't matter
-        </div>
-       
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
+          Relased 5 years ago
+        </button>
+        
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
+          Relased 10 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
+          Relased 15 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
+          Relased 20 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
+           doesn't matter
+        </button>
       </div>
     </div>
 
