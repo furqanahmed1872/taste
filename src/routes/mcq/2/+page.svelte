4221c28f9e898c99d6fb08b36e9d1b31ee94eac2
Your Name <abdullaharifsalimee@gmail.com>
2024-12-01 20:30:04 +0500

    ok

--- src/routes/mcq/2/+page.svelte
+++ src/routes/mcq/2/+page.svelte
@@ -24,13 +24,60 @@
     </div>
 
     <!-- Grid Content -->
+<<<<<<< Updated upstream
     <div class="grid grid-cols-2 grid-rows-6 2xl 2xl:w-1/2 xl:w-2/3 m-8 lg:w-3/4">
       <!-- Black Box -->
       <div class="row-span-6 bg-black opacity-70 rounded-3xl font-poiret text-5xl justify-center items-center grid p-8 text-center">
+=======
+    <div class="grid grid-cols-3 grid-rows-10 2xl 2xl:w-1/2 xl:w-2/3 m-8 lg:w-3/4  ">
+      
+      <div class="row-span-10 my-7 flex flex-col gap-4  justify-stretch ">
+        <!-- Animated Bars -->
+         <!-- Animated Bars -->
+         <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+          Relased 5 years ago
+        </button>
+        
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+          Relased 10 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+          Relased 15 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+          Relased 20 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+           doesn't matter
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+          Relased 5 years ago
+        </button>
+        
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+          Relased 10 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+          Relased 15 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+          Relased 20 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-s-xl">
+           doesn't matter
+        </button>
+       
+      </div>
+   
+      
+      <!-- Black Box -->
+      <div class="row-span-10 bg-black opacity-70 rounded-3xl font-poiret text-5xl justify-center items-center grid p-8 text-center">
+>>>>>>> Stashed changes
         “Select the Time period for movie”
       </div>
 
       <!-- Container for Red Bars -->
+<<<<<<< Updated upstream
       <div class="row-span-6 my-7 grid gap-4 overflow-hidden relative ">
         <!-- Animated Bars -->
         <div class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50 translate-x-[-100%] rounded-e-xl">
@@ -50,6 +97,43 @@
            doesn't matter
         </div>
        
+=======
+           
+      <div class="row-span-10 my-7 grid gap-4 overflow-hidden relative">
+        <!-- Animated Bars -->
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
+          Relased 5 years ago
+        </button>
+        
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
+          Relased 10 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
+          Relased 15 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
+          Relased 20 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
+           doesn't matter
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
+          Relased 5 years ago
+        </button>
+        
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
+          Relased 10 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
+          Relased 15 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
+          Relased 20 years ago
+        </button>
+        <button class="animated-bar bg-custom-dark font-poiret text-2xl text-center p-3 hover:bg-opacity-100 bg-opacity-50  rounded-e-xl">
+           doesn't matter
+        </button>
+>>>>>>> Stashed changes
       </div>
     </div>
 
