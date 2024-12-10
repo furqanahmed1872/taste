01f060f7eb3e3134cb9befc71fd046f7ee0d2df4
Your Name <abdullaharifsalimee@gmail.com>
2024-12-10 19:07:20 +0500

    ok

--- src/routes/(app)/result/+page.svelte
+++ src/routes/(app)/result/+page.svelte
@@ -1,3 +1,90 @@
+<div
+  class="fixed inset-0 bg-cover bg-center"
+  style="background-image: url('https://static1.srcdn.com/wordpress/wp-content/uploads/2016/09/stephen-chow-kung-fu-hustler.jpg');"
+></div>
+
+<!-- Color Overlay -->
+<div
+  class="fixed inset-0 bg-[#0c0303] opacity-90 transition-opacity"
+></div>
+
+<!-- Main Content -->
+<div class="relative z-10 grid text-white h-screen">
+  <!-- Main Text -->
+  <div
+    class="relative text-4xl z-10 bg-gradient-to-r from-[#fff0] h-fit via-sky-50 font-semibold flex justify-center font-poiret text-black opacity-70 w-full text-center p-4"
+  >
+    <!-- Centered Logo -->
+    <img
+      src="../logo.png"
+      alt="Logo"
+      class="absolute top-1/2 left-1/4 transform -translate-x-1/2 -translate-y-1/2 h-20 z-10 opacity-90"
+    />
+    <!-- Text -->
+    <div class="relative z-10 flex items-center justify-center">
+      "Uncover your taste"
+    </div>
+  </div>
+
+
+<div class="bg-[#080D15] opacity-70 h-fit grid grid-cols-4 grid-rows-2 p-4  justify-center  items-center my-auto">
+  <img src="https://images.moviesanywhere.com/6add024917fe526456f0f3e7730c6133/d3ec9846-3601-4b74-ac2e-f37c9eb2c8c4.jpg" alt="" class="h-60 w-auto rounded-lg row-span-2 mx-auto">
+
+<div class="font-poiret text-xl font-medium col-span-3" >When the hapless Sing and his dim-witted pal Bone try to scam the residents of Pig Sty Alley into thinking they're members of the dreaded Axe Gang, the real gangsters descend on this Shanghai slum to restore their fearsome reputation. What gang leader Brother Sum doesn't know is that three legendary retired kung fu masters live anonymously in this decrepit neighborhood and don't take kindly to interlopers.</div>
+<div class="flex col-span-3">
+  <button
+    class="z-10 w-fit bg-black opacity-70 m-2 h-fit rounded-xl font-poiret text-2xl text-white text-right flex justify-center items-center p-2"
+  >
+    <img src="../imdb.png" alt="" class="h-8 w-fit mx-2" />
+    <div class="mx-2">9.8</div>
+  </button>
+  <button
+    class="z-10 w-fit bg-white opacity-50 m-2 h-fit p-2 rounded-xl font-poiret text-2xl text-black"
+  >
+    Action
+  </button>
+  <button
+    class="z-10 w-fit bg-cyan-900 m-2 h-fit rounded-xl p-2 font-poiret text-2xl text-white"
+  >
+    Comedy
+  </button>
+  <button
+    class="z-10 w-fit bg-custom-dark m-2 h-fit rounded-xl p-2 font-poiret text-2xl text-white"
+  >
+    2004
+  </button>
+  <button
+    class="z-10 w-fit bg-white mx-5 my-2 h-fit rounded-xl p-2 font-bold font-poiret text-3xl text-black ml-auto"
+  >
+    MORE
+  </button>
+</div>
+
+</div>
+
+<div
+class="flex w-fit h-fit bg-white p-2 rounded-xl justify-center items-center mx-auto my-auto" 
+>
+<button
+
+  class=" rounded-full font-poiret bg-black w-12 h-12 text-lg flex justify-center items-center mx-2"
+
+  >Join</button
+>
+<div class="text-black font-poiret text-xl ">
+  you just found your community want to talk to like you <b
+    class="text-red-800">“KUNFAN”</b
+  >
+</div>
+</div>
+
+
+
+</div>
+
+
+
+
 <!-- <div class="relative w-full min-h-screen">
   <div
     class="fixed inset-0 bg-cover bg-center"
@@ -39,18 +126,18 @@
       </div>
       <div class="row-span-3 col-span-2"></div>
       <button
-        class="z-10 row-span-1 col-span-2 bg-black opacity-70 m-2 rounded-xl font-poiret text-5xl text-white text-right flex justify-center items-center p-2"
+        class=w-fit"z-10  bg-black opacity-70 m-2 rounded-xl font-poiret text-5xl text-white text-right flex justify-center items-center p-2"
       >
         <img src="../imdb.png" alt="" class="h-12 w-fit mx-2" />
         <div class="mx-2">9.8</div>
       </button>
       <button
-        class="z-10 row-span-1 col-span-2 bg-white opacity-50 m-2 rounded-xl font-poiret text-5xl text-black"
+        class=w-fit"z-10  bg-white opacity-50 m-2 rounded-xl font-poiret text-5xl text-black"
       >
         Action
       </button>
       <button
-        class="z-10 row-span-1 col-span-2 bg-custom-dark m-2 rounded-xl font-poiret text-5xl text-white"
+        class=w-fit"z-10  bg-custom-dark m-2 rounded-xl font-poiret text-5xl text-white"
       >
         More
       </button>
@@ -61,7 +148,7 @@
     >
       <button
 
-        class="bg-black rounded-full font-poiret w-16 h-16 text-2xl flex justify-center items-center mx-2"
+        clasw-fits="bg-black rounded-full font-poiret w-16 h-16 text-2xl flex justify-center items-center mx-2"
 
         >Join</button
       >
