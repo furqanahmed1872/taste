650571bee84584210e60e62786c2788733db352c
Dev <98402746+furqanahmed1872@users.noreply.github.com>
2024-12-30 20:35:19 +0500

    ok

---
+++ src/routes/(app)/about/+page.svelte
@@ -0,0 +1,58 @@
+<div class="min-h-screen bg-gray-50 p-4 sm:p-6 lg:px-8">
+  <div class="relative w-full overflow-hidden">
+    <div
+      class="fixed inset-0 bg-cover bg-black/50 bg-center"
+      style="background-image: url('../back.jpg');"
+    ></div>
+
+    <div
+      class="fixed inset-0 bg-gradient-to-r from-[#051721] via-black to-[#210505] opacity-95 transition-opacity"
+    ></div>
+
+    <div class="relative z-10 flex flex-col justify-around gap-4 items-center text-white">
+      <div class="flex justify-center my-4">
+        <img src="../logo.png" alt="Logo" class="h-24 w-24" />
+      </div>
+      <div class="max-w-3xl mx-auto">
+        <div class="max-w-4xl text-center px-6">
+          <h2
+            class="relative text-2xl my-2 md:text-4xl font-extrabold text-center bg-gradient-to-r from-black via-white to-black bg-clip-text text-transparent mb-8 animate-gradient-slide"
+          >
+            About Us
+          </h2>
+          <p class="mt-6 text-lg md:text-xl leading-relaxed">
+            At <span class="text-yellow-400 font-semibold">Movie World</span>,
+            we are passionate about bringing the magic of cinema to your screen.
+            From timeless classics to the latest blockbusters, our mission is to
+            celebrate storytelling in all its forms. Whether you're a fan of
+            action, drama, comedy, or sci-fi, there's something for everyone
+            here.
+          </p>
+          <p class="mt-4 text-lg md:text-xl leading-relaxed">
+            Discover your next favorite film, connect with fellow movie
+            enthusiasts, and explore the rich history of cinema.
+            <span class="font-semibold text-yellow-400">Movie World</span> is your
+            ultimate destination for all things movies.
+          </p>
+          <div class="flex justify-center space-x-4 mt-8">
+            <div
+              class="w-12 h-12 flex items-center justify-center rounded-full bg-gray-700 hover:bg-yellow-400 text-yellow-400 hover:text-black transition duration-300 text-2xl"
+            >
+              🎥
+            </div>
+            <div
+              class="w-12 h-12 flex items-center justify-center rounded-full bg-gray-700 hover:bg-yellow-400 text-yellow-400 hover:text-black transition duration-300 text-2xl"
+            >
+              🍿
+            </div>
+            <div
+              class="w-12 h-12 flex items-center justify-center rounded-full bg-gray-700 hover:bg-yellow-400 text-yellow-400 hover:text-black transition duration-300 text-2xl"
+            >
+              ⭐
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
